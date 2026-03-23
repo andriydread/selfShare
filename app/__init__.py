db002ed0c5783d81a4e5dd4ad985ab3f0a3cb7c3
Andrii <dikoua9917@gmail.com>
2026-03-23 11:45:55 +0200

    feat: implement secure file upload logic to disk and database

--- app/__init__.py
+++ app/__init__.py
@@ -9,13 +9,19 @@ db = SQLAlchemy()
 
 def create_app():
     dotenv.load_dotenv()
+
     app = Flask(__name__)
+
+    app.config["UPLOAD_FOLDER"] = os.path.join(os.getcwd(), "uploads")
+    os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+
     db.init_app(app)
     with app.app_context():
         from . import models  # noqa
 
         db.create_all()
+
     from .routes import api_bp
 
     app.register_blueprint(api_bp)
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,40 @@
-from flask import Blueprint, jsonify
+import os
+import uuid
+from datetime import datetime, timedelta, timezone
+
+from flask import Blueprint, current_app, jsonify, request
+from werkzeug.utils import secure_filename
+
+from . import db
+from .models import File
 
 api_bp = Blueprint("api", __name__, url_prefix="/api/v1")
 
 
 @api_bp.route("/files", methods=["POST"])
 def upload_file():
-    return jsonify({"message": "Upload endpoint hit!"}), 201
+
+    if "file" in request.files:
+        uploaded_file = request.files["file"]
+
+        if uploaded_file.filename == "":
+            return jsonify({"error": "No selected file"}), 400
+
+        file_id = str(uuid.uuid4())
+        save_path = os.path.join(current_app.config["UPLOAD_FOLDER"], file_id)
+        uploaded_file.save(save_path)
+        expires = datetime.now(timezone.utc) + timedelta(hours=24)
+
+        new_file = File(
+            id=file_id,
+            original_filename=secure_filename(uploaded_file.filename),
+            storage_path=save_path,
+            expires_at=expires,
+        )
+        db.session.add(new_file)
+        db.session.commit()
+
+        return jsonify({"success": "File saved", "file_id": file_id}), 201
+
+    else:
+        return jsonify({"error": "No file provided"}), 400
